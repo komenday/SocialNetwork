65d1cd54deaec9029c560707e3f56e219c516490
Yurii Komenda <yurii.komenda@dataart.com>
2023-05-24 16:19:08 +0300

    created ICommandDispatcher interface

---
+++ SQRS-ES/SQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using SQRS.Core.Commands;
+
+namespace SQRS.Core.Infrastructure;
+
+public interface ICommandDispatcher
+{
+    void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+    Task SendAsync(BaseCommand command);
+}
