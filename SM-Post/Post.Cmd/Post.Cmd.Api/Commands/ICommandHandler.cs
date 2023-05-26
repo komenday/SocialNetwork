d664cea2de7098565893326320f22c651b0745a4
Yurii Komenda <yurii.komenda@dataart.com>
2023-05-26 16:33:57 +0300

    Command Handler

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Commands/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Post.Cmd.Api.Commands;
+
+public interface ICommandHandler
+{
+    Task HandleAsync(NewPostCommand command);
+    Task HandleAsync(EditMessageCommand command);
+    Task HandleAsync(LikePostCommand command);
+    Task HandleAsync(AddCommentCommand command);
+    Task HandleAsync(EditCommentCommand command);
+    Task HandleAsync(RemoveCommentComand command);
+    Task HandleAsync(DeletePostCommand command);
+}
