16085fc2fb718b14e2ecef5a3782347db00b88bd
Yurii Komenda <yurii.komenda@dataart.com>
2023-05-29 19:21:41 +0300

    DbContext and DbContextFactory

--- SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
+++ SM-Post/Post.Query/Post.Query.Infrastructure/DataAccess/DatabaseContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Entities;
 
 namespace Post.Query.Infrastructure.DataAccess;
 
 public class DatabaseContext : DbContext
 {
+    public DbSet<PostEntity> Posts { get; set; }
+    public DbSet<CommentEntity> Comments { get; set; }
 
+    public DatabaseContext(DbContextOptions options): base(options)
+    {
+        Database.EnsureCreated();
+    }
 }
