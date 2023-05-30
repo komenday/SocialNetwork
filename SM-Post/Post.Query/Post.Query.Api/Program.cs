808d2c200ae98cd1ec469c7f7d3ef426a9f2abe8
Yurii Komenda <yurii.komenda@dataart.com>
2023-05-30 12:30:43 +0300

    IEventHandler, EventHandler

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Post.Query.Domain.Repositories;
 using Post.Query.Infrastructure.DataAccess;
+using Post.Query.Infrastructure.Handlers;
+using Post.Query.Infrastructure.Repositories;
+using EventHandler = Post.Query.Infrastructure.Handlers.EventHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +14,10 @@ Action<DbContextOptionsBuilder> contextConfig =
 
 builder.Services.AddDbContext<DatabaseContext>(contextConfig);
 builder.Services.AddSingleton(new DatabaseContextFactory(contextConfig));
+
+builder.Services.AddScoped<IPostRepository, PostRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<IEventHandler, EventHandler>();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
