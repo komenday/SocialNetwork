787d91f444ba7e2f6910ad31eb3da362f4dd5047
Yurii Komenda <yurii.komenda@dataart.com>
2023-05-31 11:12:14 +0300

    EventConsumer and ConsumerHosteService

--- SM-Post/Post.Query/Post.Query.Api/Program.cs
+++ SM-Post/Post.Query/Post.Query.Api/Program.cs
@@ -1,8 +1,11 @@
+using Confluent.Kafka;
 using Microsoft.EntityFrameworkCore;
 using Post.Query.Domain.Repositories;
+using Post.Query.Infrastructure.Consumers;
 using Post.Query.Infrastructure.DataAccess;
 using Post.Query.Infrastructure.Handlers;
 using Post.Query.Infrastructure.Repositories;
+using SQRS.Core.Consumers;
 using EventHandler = Post.Query.Infrastructure.Handlers.EventHandler;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,12 +15,16 @@ var builder = WebApplication.CreateBuilder(args);
 Action<DbContextOptionsBuilder> contextConfig = 
     o => o.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
 
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection(nameof(ConsumerConfig)));
+
 builder.Services.AddDbContext<DatabaseContext>(contextConfig);
 builder.Services.AddSingleton(new DatabaseContextFactory(contextConfig));
 
 builder.Services.AddScoped<IPostRepository, PostRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IEventHandler, EventHandler>();
+builder.Services.AddScoped<IEventConsumer, EventConsumer>();
+builder.Services.AddHostedService<ConsumerHostedService>();
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Consumers/ConsumerHostedService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SQRS.Core.Consumers;
+
+namespace Post.Query.Infrastructure.Consumers;
+
+public class ConsumerHostedService : IHostedService
+{
+    private readonly ILogger<ConsumerHostedService> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+    public ConsumerHostedService(ILogger<ConsumerHostedService> logger, IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Event consumer service is running");
+
+        using IServiceScope scope = _serviceProvider.CreateScope();
+        var eventConsumer = scope.ServiceProvider.GetService<IEventConsumer>();
+        var topic = Environment.GetEnvironmentVariable("KAFKA_TOPIC");
+
+        Task.Run(() => eventConsumer.Consume(topic), cancellationToken);
+
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Event consumer service is stopped");
+
+
+        return Task.CompletedTask;
+    }
+}
