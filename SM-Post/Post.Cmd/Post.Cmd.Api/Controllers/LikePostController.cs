d30528d0e51561d3c82a0d758e8a68c4f47373d9
Yurii Komenda <yurii.komenda@dataart.com>
2023-06-01 12:46:20 +0300

    LikePostController

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/LikePostController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+using SQRS.Core.Exceptions;
+using SQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class LikePostController : ControllerBase
+{
+    private readonly ILogger<LikePostController> _logger;
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public LikePostController(ILogger<LikePostController> logger, ICommandDispatcher commandDispatcher)
+    {
+        _logger = logger;
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> LikePostAsync(Guid id)
+    {
+        try
+        {
+            await _commandDispatcher.SendAsync(new LikePostCommand { Id = id });
+
+            return Ok(new BaseResponse
+            {
+                Message = "Like post request is completed successfully"
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Client made a bad request");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (AggregateNotFoundException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Could not retrieve an aggregate, client passed an incorrect post ID");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            const string SafeErrorMessage = "Error while processing request to like a post";
+            _logger.Log(LogLevel.Error, ex, SafeErrorMessage);
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = SafeErrorMessage
+            });
+        }
+    }
+}
