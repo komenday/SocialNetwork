a09e35696ceea4ffed38e931bf4448c2e8a2b662
Yurii Komenda <yurii.komenda@dataart.com>
2023-06-02 09:56:16 +0300

    RemoveCommentController

---
+++ SM-Post/Post.Cmd/Post.Cmd.Api/Controllers/RemoveCommentController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Post.Cmd.Api.Commands;
+using Post.Common.DTOs;
+using SQRS.Core.Exceptions;
+using SQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Api.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class RemoveCommentController : ControllerBase
+{
+    private readonly ILogger<RemoveCommentController> _logger;
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public RemoveCommentController(ILogger<RemoveCommentController> logger, ICommandDispatcher commandDispatcher)
+    {
+        _logger = logger;
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> RemoveCommentAsync(Guid id, RemoveCommentComand command)
+    {
+        try
+        {
+            command.Id = id;
+            await _commandDispatcher.SendAsync(command);
+
+            return Ok(new BaseResponse
+            {
+                Message = "Remove comment request complete successfully!"
+            });
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Client made a bad request");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (AggregateNotFoundException ex)
+        {
+            _logger.Log(LogLevel.Warning, ex, "Could not retrieve an aggregate, client passed an incorrect post ID");
+            return BadRequest(new BaseResponse
+            {
+                Message = ex.Message
+            });
+        }
+        catch (Exception ex)
+        {
+            const string SafeErrorMessage = "Error while processing request to remove a comment from a post";
+            _logger.Log(LogLevel.Error, ex, SafeErrorMessage);
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = SafeErrorMessage
+            });
+        }
+    } 
+}
